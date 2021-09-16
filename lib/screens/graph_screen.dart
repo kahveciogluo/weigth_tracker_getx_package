be560e27db3f65fb336da175410470dd51bd8bb9
Özlem Kahvecioğlu <kahveciogluozlem@gmail.com>
2021-09-16 11:29:35 +0300

    nothing changed.

--- lib/screens/graph_screen.dart
+++ lib/screens/graph_screen.dart
@@ -8,11 +8,13 @@ class GraphScreen extends StatefulWidget {
 }
 
 class _GraphScreenState extends State<GraphScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Graph'),centerTitle: true,),
-      body: Center(child: Text('Graph Screen'),),
+      appBar: AppBar(title: Text('Graph'), centerTitle: true,),
+      body: Center(child: Text('Graph Screen'),
+      ),
     );
   }
 }
