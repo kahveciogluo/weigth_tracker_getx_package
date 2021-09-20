b8b2c3d543755d6edaf00b7e591910e7cf3069ec
Özlem Kahvecioğlu <kahveciogluozlem@gmail.com>
2021-09-20 23:53:59 +0300

    the controller was updated.

--- lib/screen-models/controller.dart
+++ lib/screen-models/controller.dart
@@ -4,10 +4,10 @@ import 'package:weight_tracker_getx_package/models/record.dart';
 class Controller extends GetxController{
 
   var records = <Record>[
-    Record(weigth: 80, dateTime: DateTime.now(), note: 'Note beee'),
+   /* Record(weigth: 80, dateTime: DateTime.now(), note: 'Note beee'),
     Record(weigth: 60, dateTime: DateTime.now(),note: 'BBB'),
     Record(weigth: 30, dateTime: DateTime.now(),note: 'CCC'),
-    Record(weigth: 50, dateTime: DateTime.now(),note: 'DDD'),
+    Record(weigth: 50, dateTime: DateTime.now(),note: 'DDD'),*/
 
   ].obs ;
 
